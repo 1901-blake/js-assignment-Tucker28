1b11a737dcf0dc0d3615b517b8eb7886de48af17
Tucker28 <tucker28@gmail.com>
2019-01-22 11:36:59 -0500

    Upload palandrome

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,23 +2,23 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-    someStr = someStr.toString();
-    someStr = someStr.toLowerCase();
-    let array = someStr.split("");
-    let revArray = array.slice(0);
-    revArray.reverse();
+    someStr = someStr.toString();                   // converts to string so that it can handle numbers
+    someStr = someStr.toLowerCase();                // converts to lower case to guard against things like R !== r
+    let array = someStr.split("");                  // splits strings by character and inserting into array
+    let revArray = array.slice(0);                  // generates new array
+    revArray.reverse();                             // reverses new array
 
-    arrayStr = '';
+    arrayStr = '';                                  // generates string from original array
     for (i in array) {
         arrayStr = arrayStr + array[i];
     }
 
-    revArrayStr = '';
+    revArrayStr = '';                               // generates string from reverse array
     for (i in revArray) {
         revArrayStr = revArrayStr + revArray[i];
     }
 
-    if (arrayStr === revArrayStr) {
+    if (arrayStr === revArrayStr) {                 // compare both arrays
         console.log(true);
     } else {
         console.log(false);
