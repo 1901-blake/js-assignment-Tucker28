9d3af6283a8fffc25b7774e2c132f831ac46e910
Tucker28 <tucker28@gmail.com>
2019-01-22 11:10:43 -0500

    Upload Bubble Sort

--- problems/02-bubble-sort/02-bubble-sort.js
+++ problems/02-bubble-sort/02-bubble-sort.js
@@ -3,15 +3,15 @@ Define function: bubbleSort(numArray)
 Use the bubble sort algorithm to sort the array.
 Return the sorted array. */
 function bubbleSort(numArray) {
-    let sorted = false;
-    while (!sorted) {
-        sorted = true;
+    let sorted = false;                                 // set sorted to false for the while loop
+    while (!sorted) {                                   // runs as long as sorted is false
+        sorted = true;                                  // sets sorted to true in case the following if statement is never triggered
         for (let i = 0; i < numArray.length; i++) {
-            if (numArray[i] > numArray[i+1]) {
+            if (numArray[i] > numArray[i+1]) {          // if two elements aren't in the right order, swap them
                 let temp = numArray[i];
                 numArray[i] = numArray[i+1];
                 numArray[i+1] = temp;
-                sorted = false;
+                sorted = false;                         // set sorted to false since this statement was triggered
             }
         }
     }
