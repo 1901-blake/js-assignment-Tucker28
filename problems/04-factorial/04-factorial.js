84acd517fda641d406fee6cf6e3cde25d1583371
Tucker28 <tucker28@gmail.com>
2019-01-22 11:24:16 -0500

    Upload Factorial

--- problems/04-factorial/04-factorial.js
+++ problems/04-factorial/04-factorial.js
@@ -5,15 +5,16 @@ Use recursion to compute and return the factorial of someNum.  */
 let array = [];
 
 function factorial(sumNum) {
-    if (sumNum % 1 !== 0) {
+    // let array = [];
+    if (sumNum % 1 !== 0) {                                                                         // error catching in case number passed in is not an integer
         console.log(`Factorial's only work on integers.  Please enter an integer and try again.`);
-    } else if (sumNum > 0) {
+    } else if (sumNum > 0) {                                                                        // if integer, push it to the array and then recursively call function
         array.push(sumNum);
         sumNum--;
         factorial(sumNum);
-    } else {
+    } else {                                                                                        // once array is complete, generates factorial
         let sumFactorial = 1;
-        //console.log(array);
+        // console.log(array);
         for (i in array) {
             sumFactorial = sumFactorial * array[i];
         }
